a243d3b8c4700dc9cb14c6b04a089578f48852b9
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 15:04:12 +0100

    test

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -38,7 +38,7 @@
         <link rel="stylesheet" href="/resources/demos/style.css">
         <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
         <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
-        <script>
+        <script> 
             $( function() {
                 $( "#datepicker" ).datepicker();
             } );
