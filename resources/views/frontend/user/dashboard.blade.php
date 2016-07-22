8b9e76db700d442af8126bd25804c3a1ea6bf2a9
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 12:01:21 +0100

    test

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -16,6 +16,12 @@
                             <li role="presentation" class="active">
                                 <a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">{{ trans('navs.frontend.user.my_information') }}</a>
                             </li>
+                            <li role="presentation">
+                                <a href="#address" aria-controls="address" role="tab" data-toggle="tab">Address</a>
+                            </li>
+                            <li role="presentation">
+                                <a href="#reference" aria-controls="address" role="tab" data-toggle="tab">Reference Details</a>
+                            </li>
                         </ul>
 
                         <div class="tab-content">
@@ -54,6 +60,41 @@
                                     </tr>
                                 </table>
                             </div><!--tab panel profile-->
+
+                            <div role="tabpanel" class="tab-pane" id="address">
+                                <table class="table table-striped table-hover table-bordered dashboard-table">
+                                    <tr>
+                                        <th>{{ trans('labels.frontend.user.profile.avatar') }}</th>
+                                        <td><img src="{{ $user->picture }}" class="user-profile-image" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th>{{ trans('labels.frontend.user.profile.name') }}</th>
+                                        <td>{{ $user->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>{{ trans('labels.frontend.user.profile.email') }}</th>
+                                        <td>{{ $user->email }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>{{ trans('labels.frontend.user.profile.created_at') }}</th>
+                                        <td>{{ $user->created_at }} ({{ $user->created_at->diffForHumans() }})</td>
+                                    </tr>
+                                    <tr>
+                                        <th>{{ trans('labels.frontend.user.profile.last_updated') }}</th>
+                                        <td>{{ $user->updated_at }} ({{ $user->updated_at->diffForHumans() }})</td>
+                                    </tr>
+                                    <tr>
+                                        <th>{{ trans('labels.general.actions') }}</th>
+                                        <td>
+                                            {{ link_to_route('frontend.user.profile.edit', trans('labels.frontend.user.profile.edit_information'), [], ['class' => 'btn btn-primary btn-xs']) }}
+
+                                            @if ($user->canChangePassword())
+                                                {{ link_to_route('auth.password.change', trans('navs.frontend.user.change_password'), [], ['class' => 'btn btn-warning btn-xs']) }}
+                                            @endif
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div><!--tab panel address-->
 
                         </div><!--tab content-->
 
