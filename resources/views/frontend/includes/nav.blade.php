766020d2ba53652f363c971e50358ae9123e076a
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 10:37:23 +0100

    test

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -55,12 +55,12 @@
                 @endif
             </ul>
             <div id="navbar" class="navbar-collapse collapse">
-                <form class="navbar-form navbar-right" action="/auth/login">
+                {{ Form::open(['route' => 'auth.login', 'class' => 'form-horizontal']) }}
                     <div class="form-group">
-                        <input type="text" placeholder="Email" class="form-control">
+                        <input name="email" type="text" placeholder="Email" class="form-control">
                     </div>
                     <div class="form-group">
-                        <input type="password" placeholder="Password" class="form-control">
+                        <input name="password" type="password" placeholder="Password" class="form-control">
                     </div>
                     <button type="submit" class="btn btn-success">Sign in</button>
                 </form>
