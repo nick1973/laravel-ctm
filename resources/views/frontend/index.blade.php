7fa273605ea9d9fd4810ce10beb65dd9af900f26
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 11:21:51 +0100

    test

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -74,19 +74,19 @@
 
     <div class="row">
 
-        <div class="col-md-10 col-md-offset-1">
+        {{--<div class="col-md-10 col-md-offset-1">--}}
 
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-home"></i> {{ trans('navs.general.home') }}
-                </div>
+            {{--<div class="panel panel-default">--}}
+                {{--<div class="panel-heading">--}}
+                    {{--<i class="fa fa-home"></i> {{ trans('navs.general.home') }}--}}
+                {{--</div>--}}
 
-                <div class="panel-body">
-                    {{ trans('strings.frontend.welcome_to', ['place' => app_name()]) }}
-                </div>
-            </div><!-- panel -->
+                {{--<div class="panel-body">--}}
+                    {{--{{ trans('strings.frontend.welcome_to', ['place' => app_name()]) }}--}}
+                {{--</div>--}}
+            {{--</div><!-- panel -->--}}
 
-        </div><!-- col-md-10 -->
+        {{--</div><!-- col-md-10 -->--}}
 
         {{--@role('Administrator')--}}
             {{-- You can also send through the Role ID --}}
