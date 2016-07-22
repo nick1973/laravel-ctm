2d42a8b6b3c6e7c542965a566616f5718fcdff25
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 12:21:04 +0100

    test

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -12,11 +12,12 @@
         </div><!--navbar-header-->
 
         <div class="collapse navbar-collapse" id="frontend-navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li>{{ link_to_route('frontend.index', trans('navs.general.home')) }}</li>
-                {{--<li>{{ link_to_route('frontend.macros', trans('navs.frontend.macros')) }}</li>--}}
-            </ul>
-
+            @if(access()->guest())
+                <ul class="nav navbar-nav">
+                    <li>{{ link_to_route('frontend.index', 'Register') }}</li>
+                    {{--<li>{{ link_to_route('frontend.macros', trans('navs.frontend.macros')) }}</li>--}}
+                </ul>
+            @endif
             <ul class="nav navbar-nav navbar-right">
                 {{--@if (config('locale.status') && count(config('locale.languages')) > 1)--}}
                     {{--<li class="dropdown">--}}
