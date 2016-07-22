a2403995fa82fa7bd4619aa4bb4a72e6e2c1f946
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 15:28:20 +0100

    test

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -38,9 +38,9 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/js/bootstrap-datepicker.min.js"></script>
 
         <script>
-            $( document ).ready(function() {
+
                 $('.datepicker').datepicker();
-            });
+
         </script>
 
     </head>
