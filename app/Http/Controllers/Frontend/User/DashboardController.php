3708ff98d1a8cb04f6cb20d91edc302671309d40
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 17:14:57 +0100

    test

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -15,6 +15,7 @@ class DashboardController extends Controller
     /**
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
+    
     public function index()
     {
         return view('frontend.user.dashboard')
