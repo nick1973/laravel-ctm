f4aa06107f3315ccdb881795bf226a385114d39f
Nick Ashford <Nick@ctm.uk.com>
2016-07-22 12:12:54 +0100

    test

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
 
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12 col-lg-12">
 
             <div class="panel panel-default">
                 <div class="panel-heading">{{ trans('navs.frontend.dashboard') }}</div>
