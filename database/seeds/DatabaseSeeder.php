0afc3d61ba108e30cccdf501ecd309b698eb52ab
Nick Ashford <Nick@ctm.uk.com>
2016-12-05 10:05:36 +0000

    new registration and text proof of concept

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AccessTableSeeder::class);
         $this->call(HistoryTypeTableSeeder::class);
         $this->call(PayGradesTableSeeder::class);
+        $this->call(NotesTableSeeder::class);
 
         Model::reguard();
     }
