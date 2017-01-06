2cd3648eb93d0e4046f9be2bec642ca430eb4018
nick1973 <nick1973_5@btinternet.com>
2017-01-06 15:54:24 +0000

    d

--- app/Http/Controllers/Frontend/User/ProfileController.php
+++ app/Http/Controllers/Frontend/User/ProfileController.php
@@ -356,6 +356,7 @@ class ProfileController extends Controller
         //Storage::disk('volume')->put('docs/' . $user->name . $user->lastname . '.' . $user->dob .'/passport_photo_page/'.$file_name, $file);
         //Storage::put('payroll/test1.txt', $result[0]);
         $file_path = $file->move('~/mnt/volume-1/' . $user->name . $user->lastname . '.' . $user->dob .'/passport_photo_page', $file_name);
+        return $file_path;
         //Storage::move('docs/' . $user->name . $user->lastname . '.' . $user->dob .'/passport_photo_page'. '/' . $file_name, $file_name);
         References::where('user_id', $user->id)->update(['passport_photo_page' => $file_path]);
         $path = References::find(1);
