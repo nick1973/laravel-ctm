2ad7490540b55a47064b004512f6cc0cc5b1a831
nick1973 <nick1973_5@btinternet.com>
2017-01-07 13:50:37 +0000

    yu

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -29,9 +29,10 @@ Route::get('/doc', function () {
             //ob_clean();   // discard any data in the output buffer (if possible)
             //flush();      // flush headers (if possible)
 //            $file = Storage::get('payroll/'.$file);
-            //return (new \Illuminate\Http\Response($file, 200));
+            return (new \Illuminate\Http\Response($file, 200))
+                ->header('Content-Type', 'application/octet-stream');
 
-    return response()->download($file);
+    //return response()->download($path);
     
 });
 
