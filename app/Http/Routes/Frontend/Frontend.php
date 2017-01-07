e86a711802b64efd54b9fa41e942a2b45e6cc2e9
nick1973 <nick1973_5@btinternet.com>
2017-01-07 13:01:17 +0000

    dd

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -22,7 +22,7 @@ Route::get('/events', function () {
 
 Route::get('/doc', function () {
     //return ['data' => \App\Models\Ops\Events::all()];
-    return Storage::disk('volume/NickAshford.1988-01-20/ni_card')->exists('AboutUs.jpg');//get('file.jpg');
+    return Storage::disk('volume')->exists('/NickAshford.1988-01-20/ni_card/AboutUs.jpg');//get('file.jpg');
 });
 
 //Route::get('/amazon_docs', function () {
