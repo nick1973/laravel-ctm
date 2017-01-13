20477caafebaaf0225057f7a4e7a618dc02b03f8
nick1973 <nick1973_5@btinternet.com>
2017-01-13 10:20:01 +0000

    hjhju

--- database/seeds/UserRollsAllUsersSeeder.php
+++ database/seeds/UserRollsAllUsersSeeder.php
@@ -12,11 +12,38 @@ class UserRollsAllUsersSeeder extends Seeder
      */
     public function run()
     {
+        if (DB::connection()->getDriverName() == 'mysql') {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        }
+
+        if (DB::connection()->getDriverName() == 'mysql') {
+            DB::table(config('access.assigned_roles_table'))->truncate();
+        } elseif (DB::connection()->getDriverName() == 'sqlite') {
+            DB::statement('DELETE FROM ' . config('access.assigned_roles_table'));
+        } else {
+            //For PostgreSQL or anything else
+            DB::statement('TRUNCATE TABLE ' . config('access.assigned_roles_table') . ' CASCADE');
+        }
+
+        //Attach admin role to admin user
+        $user_model = config('auth.providers.users.model');
+        $user_model = new $user_model;
+        $user_model::first()->attachRole(1);
+
+        //Attach executive role to executive user
+        $user_model = config('auth.providers.users.model');
+        $user_model = new $user_model;
+        $user_model::find(2)->attachRole(2);
+
         //Attach user role to general user
         $user_model = config('auth.providers.users.model');
         $user_model = new $user_model;
         $user_model::all()->attachRole(3);
 
+        //Attach user role to OpsManager user
+        $user_model = config('auth.providers.users.model');
+        $user_model = new $user_model;
+        $user_model::find(3)->attachRole(3);
 
         if (DB::connection()->getDriverName() == 'mysql') {
             DB::statement('SET FOREIGN_KEY_CHECKS=1;');
