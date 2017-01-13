4a30d3b38dd1f6e5110a7efa8ab6822d0f74a5b2
nick1973 <nick1973_5@btinternet.com>
2017-01-13 13:42:25 +0000

    migration

---
+++ database/migrations/2017_01_13_131238_UpdateReferencesToInt.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateReferencesToInt extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('references', function (Blueprint $table) {
+            $table->integer('ref_employed_from')->nullable()->change();
+            $table->integer('ref_employed_to')->nullable()->change();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //int(11)
+        
+        Schema::table('references', function (Blueprint $table) {
+            $table->string('ref_employed_from')->nullable()->change();
+            $table->string('ref_employed_to')->nullable()->change();
+            });
+        
+    }
+}
