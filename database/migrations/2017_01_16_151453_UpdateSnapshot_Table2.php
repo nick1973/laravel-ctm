6c89f3d10344e9c3a0f1e4fc7af9653d4e9cf518
Nick Ashford <Nick@ctm.uk.com>
2017-01-16 15:15:22 +0000

    updated user Snapshot nullable

---
+++ database/migrations/2017_01_16_151453_UpdateSnapshot_Table2.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSnapshotTable2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_snapshot', function ($table) {
+            $table->text('dirty')->nullable()->change();
+            $table->text('address_dirty')->nullable()->change();
+            $table->text('reference_dirty')->nullable()->change();
+            $table->text('rtw_dirty')->nullable()->change();
+            $table->text('docs_dirty')->nullable()->change();
+            $table->string('photo')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
