e5061be47e6f73acbc445e4ee8172cbbd521ea55
Nick Ashford <Nick@ctm.uk.com>
2017-01-17 14:54:22 +0000

    updated staff search fields

--- resources/views/frontend/manager/staff.blade.php
+++ resources/views/frontend/manager/staff.blade.php
@@ -131,7 +131,7 @@
                                 //return dd+'-'+mm+'-'+yyyy;
                                 var foo = + new Date();
                                 var difference = foo - data.dob;
-                                return difference + ' ' + foo + ' ' + dob
+                                return difference + ' ' + foo + ' ' + data.dob
                                 var t = new Date(difference);
                                 return t.getMinutes()*60*60 + ' days';
                             }
