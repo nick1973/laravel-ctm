97e73f7eb558691973da4b3904d455598fdbe194
Nick Ashford <Nick@ctm.uk.com>
2017-01-17 13:41:01 +0000

    updated staff search fields

--- resources/views/frontend/manager/staff.blade.php
+++ resources/views/frontend/manager/staff.blade.php
@@ -106,16 +106,8 @@
                                 return data.dob;
                             } else
                             {
-                                var date = new Date(data.dob*1000);
-// Hours part from the timestamp
-                                var hours = date.getHours();
-// Minutes part from the timestamp
-                                var minutes = "0" + date.getMinutes();
-// Seconds part from the timestamp
-                                var seconds = "0" + date.getSeconds();
-
-// Will display time in 10:30:23 format
-                                return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                                var date = new Date(data.dob);
+                                return date.format("dd-mm-yyyy");
                             }
                         }
                     },
