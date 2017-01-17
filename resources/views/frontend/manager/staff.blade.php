300a709c5bf9b5d2a89b3d4641346ec7dc5d8509
Nick Ashford <Nick@ctm.uk.com>
2017-01-17 11:44:14 +0000

    updated staff search fields

--- resources/views/frontend/manager/staff.blade.php
+++ resources/views/frontend/manager/staff.blade.php
@@ -93,7 +93,7 @@
                     { "data": "payroll" , className: "centre get" },
                     { "data": "name" , className: "centre get" },
                     { "data": "lastname" , className: "centre get" },
-                    { "targets": 0, },
+                    { "data": null },
                     { "data": "mobile" , className: "centre get" },
                     { "data": "email" , className: "centre get" },
                     { "data": "profile_confirmed" , className: "centre get" },
