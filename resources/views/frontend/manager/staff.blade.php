4b2a4dbc921ca2021af701f09c1687b5bca78310
Nick Ashford <Nick@ctm.uk.com>
2017-01-17 16:36:23 +0000

    updated staff search fields

--- resources/views/frontend/manager/staff.blade.php
+++ resources/views/frontend/manager/staff.blade.php
@@ -104,7 +104,8 @@
                         "data": function (data) {
                             if (data.dob.indexOf("-") > 1){
                                 var dob = data.dob
-
+                                var dd = Number(dob.substr(6,4))
+                                return dd
                                 var d = new Date();
                                 d.setFullYear(Number(dob.substr(6,4)), Number(dob.substr(3,2)), Number(dob.substr(0,2)));
                                 return d
