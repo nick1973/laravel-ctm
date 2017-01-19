670d8a3ff07cd2d40f801c6fdf5d3c7b29a3cb52
nick1973 <nick1973_5@btinternet.com>
2017-01-19 11:07:51 +0000

    44

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -61,7 +61,7 @@
                             {{--<li role="presentation">--}}
                                 {{--<a href="#eventslist" aria-controls="eventslist" role="tab" data-toggle="tab">Events List</a>--}}
                             {{--</li>--}}
-                            @if($user->profile_confirmed=="" || $user->profile_confirmed=="No")
+                            @if($user->profile_confirmed=="" || $user->profile_confirmed=="No" || $user->profile_confirmed=="no")
                                 <li role="presentation">
                                     <a href="#submit" aria-controls="submit" role="tab" data-toggle="tab">Submit Application</a>
                                 </li>
