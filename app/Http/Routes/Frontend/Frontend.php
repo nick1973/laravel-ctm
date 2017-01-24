9943767446fd0cb6994158606fbec775e3f5d8a1
Nick Ashford <Nick@ctm.uk.com>
2017-01-24 14:07:59 +0000

    user search

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -359,6 +359,7 @@ Route::group(['middleware' => 'auth'], function () {
 
             $staff = \App\Models\Access\User\User::
                 where([
+                ['visible', '=', 1],
                 ['app_status', '=', 3],
                 ['markAsp45', '=' ,0],
                 ['payroll','!=','0'],
