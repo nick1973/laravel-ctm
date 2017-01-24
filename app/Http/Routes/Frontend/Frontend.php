0a47459bb748be396ef752de32064ba25a2e1a4f
Nick Ashford <Nick@ctm.uk.com>
2017-01-24 09:39:54 +0000

    P45

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -355,20 +355,32 @@ Route::group(['middleware' => 'auth'], function () {
             $columns =  DB::getSchemaBuilder()->getColumnListing('users');
             unset($columns['visible']);
             $columnsNeeded = array_except($columns, [1,2,3,4,5,6,7,14,15,18,23,25,26,28,30,32,33,40,41,42,43,44,45,46,48,49,50,51,52,53,54,55,56,57,58,59]);
-            //dd($columnsNeeded);
+            dd($columnsNeeded);
 
             $staff = \App\Models\Access\User\User::where('profile_confirmed', 'yes')->where('markAsp45',0)
-                //->where(function ($query) use ($columnsNeeded) {
-//                    foreach($columnsNeeded as $column){
-//                        $query->where($column, '!=', '');
-//                        $query->orWhere($column, '!=', '0');
-//                    }
-                //})
                 ->where('title','!=','')
                 ->where('name','!=','')
                 ->where('lastname','!=','')
                 ->where('mobile','!=','')
                 ->where('land','!=','')
+                ->where('dob','!=','')
+                ->where('nationality','!=','')
+                ->where('townofbirth','!=','')
+                ->where('emergency_contact_name','!=','')
+                ->where('emergency_contact_rel','!=','')
+                ->where('emergency_contact_number','!=','')
+                ->where('emergency_contact_mobile','!=','')
+                ->where('uk_driving_license','!=','')
+                ->where('convictions','!=','')
+                ->where('medical_conditions','!=','')
+                ->where('email','!=','')
+                ->where('address_line_1','!=','')
+                ->where('address_line_2','!=','')
+                ->where('city','!=','')
+                ->where('county','!=','')
+                ->where('country','!=','')
+                ->where('postcode','!=','')
+                ->where('payroll','!=','')
             ->get();
 
 
