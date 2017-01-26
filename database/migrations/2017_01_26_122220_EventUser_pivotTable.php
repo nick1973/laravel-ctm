f3aeeeca7f6984f9974db082dad850e98089e051
nick1973 <nick1973_5@btinternet.com>
2017-01-26 12:56:05 +0000

    h

--- database/migrations/2017_01_26_122220_EventUser_pivotTable.php
+++ database/migrations/2017_01_26_122220_EventUser_pivotTable.php
@@ -15,11 +15,11 @@ class EventUserPivotTable extends Migration
         //Schema::rename('events', 'opsevents');
         //Schema::rename('event_list', 'events');
         Schema::create('event_user', function (Blueprint $table) {
-            $table->increments('event_id')->unsigned();
-            //$table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->integer('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
 
-            $table->increments('user_id')->unsigned();
-            //$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
