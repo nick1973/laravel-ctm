d7b1ea5e4760825c03480a0f28c3fc6f321196fe
nick1973 <nick1973_5@btinternet.com>
2017-01-27 12:40:57 +0000

    rf

--- resources/views/frontend/user/profile/edit_documents.blade.php
+++ resources/views/frontend/user/profile/edit_documents.blade.php
@@ -152,28 +152,28 @@
         Dropzone.options.passportPhoto = {
             maxThumbnailFilesize: 4,
             clickable: true,
-            acceptedFiles: '.jpg, .jpeg, .png, .image/*',
+            acceptedFiles: '.jpg, .jpeg, .png, image/*',
             addRemoveLinks: true,
             drop: false
         };
         Dropzone.options.passportPhotoPage = {
             maxThumbnailFilesize: 4,
             clickable: true,
-            acceptedFiles: '.jpg, .jpeg, .png, .image/*',
+            acceptedFiles: '.jpg, .jpeg, .png, image/*',
             addRemoveLinks: true,
             drop: false
         };
         Dropzone.options.driving_photo = {
             maxThumbnailFilesize: 4,
             clickable: true,
-            acceptedFiles: '.jpg, .jpeg, .png, .image/*',
+            acceptedFiles: '.jpg, .jpeg, .png, image/*',
             addRemoveLinks: true,
             drop: false
         };
         Dropzone.options.NICard = {
             maxThumbnailFilesize: 4,
             clickable: true,
-            acceptedFiles: '.jpg, .jpeg, .png, .image/*',
+            acceptedFiles: '.jpg, .jpeg, .png, image/*',
             addRemoveLinks: true,
             drop: false
         };
