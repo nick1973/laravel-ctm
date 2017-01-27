0c502986785b32e57f62980d4235cbd1a4301149
nick1973 <nick1973_5@btinternet.com>
2017-01-27 09:05:41 +0000

    e

--- app/Models/Dropdowns/EventsList.php
+++ app/Models/Dropdowns/EventsList.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventsList extends Model
 {
-    protected $table = 'eventslist';
+    protected $table = 'events_list';
     protected $fillable = ['event_name', 'visible', 'month'];
 
 
