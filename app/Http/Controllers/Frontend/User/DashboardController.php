45531f9c56669ed4b401602b83c771de41da5f75
Nick Ashford <Nick@ctm.uk.com>
2017-01-31 12:52:44 +0000

    Events date Asc

--- app/Http/Controllers/Frontend/User/DashboardController.php
+++ app/Http/Controllers/Frontend/User/DashboardController.php
@@ -23,7 +23,7 @@ class DashboardController extends Controller
     {
         $reference = References::where('user_id', access()->id())->get();
         $rt_work = RTWork::where('user_id', access()->id())->get();
-        $events = Tag::where('visible', 1)->orderBy('date', 'asc')->get();
+        $events = Tag::where('visible', 1)->get();   //->orderBy('date', 'asc')
 
         if(access()->hasRole('Executive'))
         {
