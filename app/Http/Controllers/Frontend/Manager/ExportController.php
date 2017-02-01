004d33f7b3931fa172af9d9d25be8507330f95b7
nick1973 <nick1973_5@btinternet.com>
2017-02-01 14:21:52 +0000

    d

--- app/Http/Controllers/Frontend/Manager/ExportController.php
+++ app/Http/Controllers/Frontend/Manager/ExportController.php
@@ -10,7 +10,7 @@ class ExportController extends Controller
 {
     function index()
     {
-        $files = Storage::files('payroll')->orderBy('desc');
+        $files = Storage::files('payroll');
 //        dd($files);
         return view('frontend.manager.exports.index', compact('files'));
     }
