2318f285b51ecc360e2b1f9e82a34e0b4fc9c7ab
nick1973 <nick1973_5@btinternet.com>
2017-02-01 13:30:56 +0000

    confirmation_code

---
+++ database/migrations/2017_02_01_132925_Update_Snapshot_Table6.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSnapshotTable6 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_snapshot', function ($table) {
+            $table->string('confirmation_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
