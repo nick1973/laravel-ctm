b84f93c5f32a1e15de292825b32e95a3c20fb6e9
nick1973 <nick1973_5@btinternet.com>
2017-02-01 08:11:16 +0000

    g

--- resources/views/frontend/includes/submit_application.blade.php
+++ resources/views/frontend/includes/submit_application.blade.php
@@ -40,7 +40,7 @@
         var gp1_tick = $('.gp1').hasClass('bg-danger');
         var gp2_tick = $(".gp2").hasClass('bg-danger');
         console.log('gp2_tick ' + gp2_tick)
-        if(myInfo==true || address_tick==true || reference_tick==true || account_tick==true && (gp1_tick==true || gp2_tick)==true){
+        if(myInfo==true || address_tick==true || reference_tick==true || account_tick==true && (gp1_tick==true && gp2_tick)==true){
             alert('Your application is not ready to submit, Please complete un-ticked sections to complete.')
             console.log('not submitted')
         } else {
