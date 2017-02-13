fb9538936bef1a48c84386b314b55b2458f43fa3
Nick Ashford <Nick@ctm.uk.com>
2017-02-13 09:54:35 +0000

    Updated Snapshot job_status

---
+++ database/migrations/2017_02_13_095323_Update_Snapshot_Table7.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSnapshotTable7 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_snapshot', function ($table) {
+            $table->string('job_status')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
