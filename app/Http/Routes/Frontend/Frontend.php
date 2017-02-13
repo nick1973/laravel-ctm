cddbc9345203d02d0b195ed0bdd7ac0424a9be6e
Nick Ashford <Nick@ctm.uk.com>
2017-02-13 17:13:19 +0000

    sortcode validation

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -419,11 +419,13 @@ Route::group(['middleware' => 'auth'], function () {
 //                ['payroll_export', '=', 1],
 //                ['payroll', '!=', 0]
 //            ])->get();
-            return \App\Models\Access\User\User::find(28528);
-            //foreach ($staff as $res){
-            //return str_replace(' ', '', decrypt($staff->account_sort_code));
+            $staff = \App\Models\Access\User\RTWork::all();
+            foreach ($staff as $res){
+                //return str_replace(' ', '', decrypt($staff->account_sort_code));
                 //return decrypt($staff->account_sort_code);
-            //}
+                return 'Work Status '.$res->work_status;
+            }
+
 
 
             //$staff =  \App\Models\Access\User\User::where('payroll','38629')->get();
