e786559e52ed3ede1e8dc0beaa8d3cae01fbdb2d
Nick Ashford <Nick@ctm.uk.com>
2017-02-14 09:55:04 +0000

    ref model fillable work_status

--- app/Models/Access/User/References.php
+++ app/Models/Access/User/References.php
@@ -14,7 +14,5 @@ class References extends Model
         'ref_employer_country', 'ref_employer_postcode',
         'ref_char_name', 'ref_char_how_know', 'ref_char_company', 'ref_char_phone_number', 'ref_character_address_line_1',
         'ref_character_address_line_2', 'ref_character_city', 'ref_character_county', 'ref_character_country', 'ref_character_postcode',
-        'd1_photo', 'passport_photo', 'birth_cert', 'ni_card', 'passport_photo_page'];
-
-
+        'd1_photo', 'passport_photo', 'birth_cert', 'ni_card', 'passport_photo_page', 'work_status'];
 }
