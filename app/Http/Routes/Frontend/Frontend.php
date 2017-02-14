cf7a922f17382eb23422111c65c3e0a15626fd81
Nick Ashford <Nick@ctm.uk.com>
2017-02-14 10:57:10 +0000

    frontend route

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -427,12 +427,12 @@ Route::group(['middleware' => 'auth'], function () {
             foreach ($staff as $result){
                 //return str_replace(' ', '', decrypt($staff->account_sort_code));
                 //return decrypt($staff->account_sort_code);
-                echo 'User ID ' . $result->id . '</br>';
+                //echo 'User ID ' . $result->id . '</br>';
                 $ref = \App\Models\Access\User\RTWork::where('user_id',$result->id);
                 //$ref->update(['work_status'=>'0']);
 
             }
-            return $ref;
+            dd($ref) ;
             //return $staff;
             //return $staff->work_status;
 
