cb6c5aab783799b766f3987726b81f9441fc72ec
Nick Ashford <Nick@ctm.uk.com>
2017-02-14 10:23:51 +0000

    frontend route

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -420,7 +420,7 @@ Route::group(['middleware' => 'auth'], function () {
 //                ['payroll', '!=', 0]
 //            ])->get();
             //$staff = \App\Models\Access\User\RTWork::where('user_id',34489)->get();
-            $staff = \App\Models\Access\User\User::where('profile_confirmed', '!=', 'Yes')->get();
+            $staff = \App\Models\Access\User\User::where('app_status', '!=', 3)->count();
             //$staff->update(['work_status'=>'0']);
             return $staff;
             foreach ($staff as $res){
