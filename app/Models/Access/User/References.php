0b583ed77dff7dd65b416cadf0a8e4d55374110a
Nick Ashford <Nick@ctm.uk.com>
2017-02-14 09:56:10 +0000

    ref model fillable work_status

--- app/Models/Access/User/References.php
+++ app/Models/Access/User/References.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class References extends Model
 {
 
-
     protected $fillable = ['user_id', 'ref_job_title', 'ref_employed_from', 'ref_employed_to', 'ref_company_name', 'ref_contact',
         'origin', 'gender', 'nationality', 'townofbirth',
         'ref_phone_number', 'ref_employer_address_line_1', 'ref_employer_address_line_2', 'ref_employer_city', 'ref_employer_county',
