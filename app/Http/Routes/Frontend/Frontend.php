5ff26783e2d749a22759f4bf1a1c67e84d02ea78
Nick Ashford <Nick@ctm.uk.com>
2017-02-27 11:10:20 +0000

    email staff

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -415,9 +415,9 @@ Route::group(['middleware' => 'auth'], function () {
         });
 
         Route::get('dashboard/manager/staff/search/id100', function () {
-            $staff= \App\Models\Access\User\User::where('payroll', 50966)->get();
-            //$staff= \App\Models\Access\User\User::find(32319);
-            //$staff->update(['dob'=>'1998-01-27']);
+            //$staff= \App\Models\Access\User\User::where('payroll', 50966)->get();
+            $staff= \App\Models\Access\User\User::find(33443);
+            $staff->update(['dob'=>'1988-04-29']);
             return $staff;
 
 
