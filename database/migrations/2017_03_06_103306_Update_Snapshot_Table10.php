3d1c88909e1683e5702ead5b084a5b541b6a78a6
Nick Ashford <Nick@ctm.uk.com>
2017-03-06 10:35:17 +0000

    email staff

---
+++ database/migrations/2017_03_06_103306_Update_Snapshot_Table10.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateSnapshotTable10 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_snapshot', function ($table) {
+            $table->string('heard_about_us')->nullable()->change();
+            $table->string('uni')->nullable()->change();
+            $table->string('promotion')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
