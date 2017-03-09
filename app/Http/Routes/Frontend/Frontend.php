de44fa7405bb318a14c87e57456225640cc64d89
Nick Ashford <Nick@ctm.uk.com>
2017-03-09 15:21:12 +0000

    d100

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -398,8 +398,8 @@ Route::group(['middleware' => 'auth'], function () {
         });
 
         Route::get('dashboard/manager/staff/search/id100', function () {
-            //$staff= \App\Models\Access\User\User::where('payroll', 50966)->get();
-            $staff= \App\Models\Access\User\User::where('app_status',1)->count();
+            $staff= \App\Models\Access\User\User::where('payroll', 51396)->get();
+            //$staff= \App\Models\Access\User\User::where('app_status',1)->count();
 //            $staff->update(['dob'=>'1988-04-29']);
             return $staff;
 
