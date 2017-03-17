7fddec7ddc77af319ffb414955d8b8091e8054e2
nick1973 <nick1973_5@btinternet.com>
2017-03-17 09:52:05 +0000

    dob on registration

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -35,9 +35,9 @@
                         </div><!--form-group-->
 
                         <div class="form-group">
-                            {{ Form::label('dob', 'Date of Birth', ['class' => 'col-md-4 control-label']) }}
+                            {{ Form::label('dob', 'Date of Birth (dd-mm-yyyy)', ['class' => 'col-md-4 control-label']) }}
                             <div class="col-md-6">
-                                {{ Form::input('date', 'dob', null, ['class' => 'form-control', 'placeholder' => 'dd/mm/yyyy']) }}
+                                {{ Form::input('date', 'dob', null, ['class' => 'form-control', 'placeholder' => 'dd-mm-yyyy']) }}
                             </div><!--col-md-6-->
                         </div><!--form-group-->
 
