61f30246f11f5535ec0e51b49386597079658d48
Nick Ashford <Nick@ctm.uk.com>
2017-03-20 12:34:43 +0000

    fix non approved

--- resources/views/frontend/manager/staff_search/index.blade.php
+++ resources/views/frontend/manager/staff_search/index.blade.php
@@ -272,12 +272,12 @@
                 var sum = []
                 $('.loaderImage').hide();
                 $.each( results.data, function( index, value ){
-                    //if(value['postcode'] !== ''){
-                        //var str = value['postcode'].replace(/\s/g,'');
-                        postcodes.push(value['postcode'])
-                    //}
+                    if(value['postcode'] !== '' || value['postcode'] !== null){
+                        var str = value['postcode'].replace(/\s/g,'');
+                        postcodes.push(str)
+                    }
                 });
-                console.log( postcodes );
+//                console.log( postcodes );
                 setTimeout(self.timeoutHandler, 750);
 
                 var table = $('#'+dTable).DataTable( {
