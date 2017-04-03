e5dff4dd9db475d23d03a81b9c00d13988d1f95f
Nick Ashford <Nick@ctm.uk.com>
2017-04-03 11:00:08 +0100

    Mapped all events

--- resources/views/frontend/manager/events/index.blade.php
+++ resources/views/frontend/manager/events/index.blade.php
@@ -7,6 +7,7 @@
         <div class="col-md-12 col-lg-12">
             <h3>Event List
                 <a href="{{ route('dashboard.events.create') }}" class="btn btn-warning">Add an Event</a>
+                <a class="openmodal btn btn-info" href="#eventsMap" data-toggle="modal">Events Map</a>
             </h3>
 
             <div class="col-md-8">
@@ -28,7 +29,7 @@
                             'method' => 'DELETE',
                             'route' => ['dashboard.events.destroy', $results->id]
                             ]) !!}
-                            <td>{{ $results->name }}</td>
+                            <td class="eventName">{{ $results->name }}</td>
                             <td>
                                 @if($results->visible==1)
                                     Visible
@@ -37,7 +38,7 @@
                                     @endif
                             </td>
                             <td>{{ $results->date }}</td>
-                            <td>{{ $results->postcode }}</td>
+                            <td class="postcode">{{ $results->postcode }}</td>
                             <td width="150px">
                                 <a href="{{ route('dashboard.events.edit', $results->id) }}" class="btn btn-primary btn-xs">Edit</a>
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
@@ -48,16 +49,88 @@
                 </table>
                 <a href="{{ route('dashboard.events.create') }}" class="btn btn-warning">Add an Event</a>
             </div>
-
-
-
-
-
-
-
     </div><!-- col-md-10 -->
 
     </div><!-- row -->
+    <style>
+        #map {
+            width: 100%;
+            height: 700px;
+        }
+    </style>
+    <div class="modal fade" id="eventsMap" role="dialog" >
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content" id="back" >
+                <div class="modal-header">
+                    <h4>Events<h4>
+                </div>
+                <div class="modal-body">
+                    <div id="map"></div>
+                </div>
+                <div class="modal-footer">
+                    <a class="btn btn-default" data-dismiss="modal">Close</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        var ev = []
+        var map;
+        var event_name = [];
+        function initMap() {
+            var infoWin = new google.maps.InfoWindow();
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 54.5, lng: -3.644},
+                zoom: 6
+            });
+
+            var markers = ev.map(function(location, i) {
+                var marker = new google.maps.Marker({
+                    position: location,
+                });
+                google.maps.event.addListener(marker, 'click', function(evt) {
+                    //infoWin.setContent(location.info);
+                    infoWin.setContent(event_name[i]);
+                    infoWin.open(map, marker);
+                })
+                return marker;
+            });
+            var markerCluster = new MarkerClusterer(map, markers,
+                    {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+        }
+
+        $('#eventsMap').on('shown.bs.modal', function (event) {
+            var post = []
+            $(".eventName").each(function (index, element) {
+                event_name.push($( this ).text())
+            })
+
+            $(".postcode").each(function (index, element) {
+                var str = $( this ).text().replace(/\s/g,'');
+                post.push(str)
+            })
+
+            //event_name = button.data('event')
+            ev = []
+            $.ajax({
+                type: "POST",
+                url: '/dashboard/manager/postcode',
+                data: {
+                    postcodes: post}
+            }).done(function(data) {
+                $.each( data.data, function( index, value ){
+                    ev.push({'lat': + value['latitude'], 'lng': + value['longitude']})
+                });
+            });
+            //console.log(ev)
+            setTimeout(function(){
+                initMap()
+                google.maps.event.trigger(map, "resize");
+            }, 1000)
+        });
+    </script>
+    <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCD7jKYXhgDTka8qlsPSqNcU2HV7DCwfUs"></script>
 
 @endsection
 
