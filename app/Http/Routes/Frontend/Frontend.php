1554060b0b659084b8e731e008273128443a0ac1
Nick Ashford <Nick@ctm.uk.com>
2017-05-25 11:53:39 +0100

    medical

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -431,7 +431,7 @@ Route::group(['middleware' => 'auth'], function () {
         });
 
         Route::get('dashboard/manager/staff/search/medical', function () {
-            $user = DB::table('users')->where('payroll', 51747)->get();
+            $user = DB::table('user_snapshot')->where('payroll', 50659)->get();
                 return $user;
         });
 
