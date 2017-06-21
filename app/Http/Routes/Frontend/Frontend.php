51397f907c1a1972a390dcb84ecdd4a5fdcdb202
nick1973 <nick1973_5@btinternet.com>
2017-06-21 13:56:53 +0100

    kihilk

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -432,7 +432,8 @@ Route::group(['middleware' => 'auth'], function () {
 
         Route::get('dashboard/manager/staff/search/medical', function () {
             //$user = DB::table('users')->where('payroll', 50659)->get();
-            $user = \App\Models\Access\User\UserSnapshot::find(230);
+            $user = \App\Models\Access\User\UserSnapshot::where('payroll', 50644)->get();
+            return $user;
                 return Crypt::decrypt($user->account_sort_code) . ' ' .Crypt::decrypt($user->account_number);
         });
 
