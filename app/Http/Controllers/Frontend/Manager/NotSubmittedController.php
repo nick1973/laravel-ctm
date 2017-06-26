d3b7a2c576bd684811494854cc9c812128897dc5
Nick Ashford <Nick@ctm.uk.com>
2017-06-26 14:33:36 +0100

    new not submitted button

--- app/Http/Controllers/Frontend/Manager/NotSubmittedController.php
+++ app/Http/Controllers/Frontend/Manager/NotSubmittedController.php
@@ -12,22 +12,19 @@ use SebastianBergmann\GlobalState\Snapshot;
 class NotSubmittedController extends Controller
 {
     function index(){
-        //$snapshot_created = UserSnapshot::all();
-        //$snapshot_created = DB::table('user_snapshot')->select('name', 'payroll', 'updated_at')->get();
-        //$application_updated = DB::table('users')->select('name', 'payroll', 'updated_at')->get();
-//        $collection = collect($application_updated)->each(function ($item, $key) {
-//            //
-//        });
         $users = DB::table('users')
-            ->leftJoin('user_snapshot', 'users.payroll', '=', 'user_snapshot.payroll')
+            ->join('user_snapshot', 'users.payroll', '=', 'user_snapshot.payroll')
             ->where('users.updated_at', '!=', null)
             ->where('users.markAsp45', '=' ,0)
             ->where('user_snapshot.updated_at', '!=', null)
             ->where('users.updated_at', '>', 'user_snapshot.updated_at')
             ->select('users.name', 'users.payroll', 'users.updated_at as user_updated', 'users.mobile', 'user_snapshot.updated_at as snapshot_created')
             ->get();
-        //dd($application_updated);
-        //return $users;
-        return view('frontend.manager.not_submitted.index', compact('users'));
+        $unique = collect($users);
+        //$unique->whereIn('payroll');
+        $unique_payroll = $unique->unique('payroll');
+//        $unique = collect($unique_payroll);
+        //return $unique_payroll;
+        return view('frontend.manager.not_submitted.index', compact('users', 'unique_payroll'));
     }
 }
--- resources/views/frontend/manager/not_submitted/index.blade.php
+++ resources/views/frontend/manager/not_submitted/index.blade.php
@@ -10,19 +10,23 @@
             <th>User Updated</th>
             <th>Snapshot Created</th>
         </tr>
-        @foreach($users as $user)
+            @foreach($unique_payroll as $user)
             <?php
                     $user_updated = \Carbon\Carbon::parse($user->user_updated)->format('d-m-Y');
                     $snapshot_created = \Carbon\Carbon::parse($user->snapshot_created)->format('d-m-Y');
                     ?>
-            <tr>
-                <td>{{ $user->payroll }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->mobile }}</td>
-                <td>{{ $user_updated }}</td>
-                <td>{{ $snapshot_created }}</td>
-            </tr>
-        @endforeach
+
+                        <tr>
+                            <td>{{ $user->payroll }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->mobile }}</td>
+                            <td>{{ $user->user_updated }}</td>
+                            <td>{{ $user->snapshot_created }}</td>
+                        </tr>
+
+
+            @endforeach
+
 
     </table>
 
