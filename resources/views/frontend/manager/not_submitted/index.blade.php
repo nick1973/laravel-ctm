a99ab0dca83e737fadeb24ab2dfa7178ff8189e4
Nick Ashford <Nick@ctm.uk.com>
2017-06-26 12:03:08 +0100

    new not submitted button

--- resources/views/frontend/manager/not_submitted/index.blade.php
+++ resources/views/frontend/manager/not_submitted/index.blade.php
@@ -7,12 +7,16 @@
             <th>Payroll</th>
             <th>Name</th>
             <th>Tel No</th>
+            <th>User Updated</th>
+            <th>Snapshot Created</th>
         </tr>
         @foreach($users as $user)
             <tr>
                 <td>{{ $user->payroll }}</td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->mobile }}</td>
+                <td>{{ $user->user_updated }}</td>
+                <td>{{ $user->snapshot_created }}</td>
             </tr>
         @endforeach
 
