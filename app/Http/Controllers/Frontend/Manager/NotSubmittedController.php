d739fe8ab6cd701539d16c5209a5e4b29bb8b195
Nick Ashford <Nick@ctm.uk.com>
2017-06-26 11:39:08 +0100

    new not submitted button

--- app/Http/Controllers/Frontend/Manager/NotSubmittedController.php
+++ app/Http/Controllers/Frontend/Manager/NotSubmittedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend\Manager;
 
+use App\Models\Access\User\User;
 use App\Models\Access\User\UserSnapshot;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,8 +13,23 @@ class NotSubmittedController extends Controller
 {
     function index(){
         //$snapshot_created = UserSnapshot::all();
-        $snapshot_created = DB::table('user_snapshot')->select('name', 'payroll')->get();
-        dd($snapshot_created);
+        $snapshot_created = DB::table('user_snapshot')->select('name', 'payroll', 'updated_at')->get();
+        //$application_updated = DB::table('users')->select('name', 'payroll', 'updated_at')->get();
+//        $collection = collect($application_updated)->each(function ($item, $key) {
+//            //
+//        });
+        $users = DB::table('users')
+            ->leftJoin('user_snapshot', 'users.payroll', '=', 'user_snapshot.payroll')
+            ->where('users.updated_at', '>', 'user_snapshot.updated_at')
+            ->select('users.name', 'users.payroll', 'users.updated_at', 'user_snapshot.updated_at')
+            ->get();
+//        foreach ($snapshot_created as $snapshot){
+//            $user_collection = collect(User::where('payroll', $snapshot->payroll)->select('name', 'payroll', 'updated_at')->get());
+//            //$snapshot_collection = collect($snapshot_created);
+//
+//        }
+        //dd($application_updated);
+        return $users;
         return view('frontend.manager.not_submitted.index');
     }
 }
