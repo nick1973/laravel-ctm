5118a98bf9fdf060c36621dd15ffd203dd416f64
nick1973 <nick1973_5@btinternet.com>
2017-07-05 16:48:19 +0100

    sss

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -442,7 +442,7 @@ Route::group(['middleware' => 'auth'], function () {
             return $staff;
             //$user = DB::table('users')->where('payroll', 50659)->get();
             $user = \App\Models\Access\User\User::where('payroll', 39982)->get();
-            return $user;
+            dd($user) ;
                 return Crypt::decrypt($user->account_sort_code) . ' ' .Crypt::decrypt($user->account_number);
         });
 
