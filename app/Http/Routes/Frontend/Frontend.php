3cd5d04e066802efcac3a2dbc60b1b5151bad0f0
Nick Ashford <Nick@ctm.uk.com>
2017-07-20 15:23:24 +0100

    payroll export dob change

--- app/Http/Routes/Frontend/Frontend.php
+++ app/Http/Routes/Frontend/Frontend.php
@@ -445,9 +445,10 @@ Route::group(['middleware' => 'auth'], function () {
                 ['payroll_export', '=', 1],
                 ['payroll', '!=', 0]
             ])->get();
+            $user = App\Models\Access\User\User::find(32628);
             //$staff->update(['dob'=>'1999-01-18']);
             //dd($staff);
-            return $staff;
+            return $user;
         });
 
         Route::get('dashboard/manager/staff/search/medical', function () {
