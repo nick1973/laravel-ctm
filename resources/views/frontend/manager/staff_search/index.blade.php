503e88ec2809798b762e52665ae9bb90455b01d7
Nick Ashford <Nick@ctm.uk.com>
2017-07-20 12:55:40 +0100

    Changed $response for text api

--- resources/views/frontend/manager/staff_search/index.blade.php
+++ resources/views/frontend/manager/staff_search/index.blade.php
@@ -527,7 +527,7 @@
                 console.log(data)
                 $("#bal").append('<p>Text Local Credits:= ' + data.message.status + '</p>'
                     + '<p>App Text sent: ' + data.number_count + '</p>'
-                    + '<p>API Text sent: ' + data.num_messages + '</p>')
+                    + '<p>API Text sent: ' + data.num_messages.length + '</p>')
             });
             //location.reload();
             //$("#confirm_form").submit();
